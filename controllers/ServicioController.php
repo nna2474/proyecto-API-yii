d9f7be2841dc69157bfb28b187aaa963739d9c4f
Victor <umihub@gmail.com>
2021-03-18 13:49:04 -0400

    cors test

--- controllers/ServicioController.php
+++ controllers/ServicioController.php
@@ -2,9 +2,28 @@
 
 namespace app\controllers;
 
+use yii\filters\Cors;
 use yii\rest\ActiveController;
 
 class ServicioController extends ActiveController
 {
     public $modelClass = 'app\models\Servicio';
+
+    public function behaviors()
+    {
+        return [
+            'corsFilter' => [
+                'class' => Cors::className(),
+                'cors' => [
+                    'Origin' => ['*'],
+                    'Access-Control-Request-Method' => ['POST', 'GET', 'PUT', 'UPDATE', 'DELETE', 'CREATE'],
+                    'Access-Control-Request-Headers' => ['Authorization', 'Content-type', 'Credentials'],
+                    'Access-Control-Allow-Credentials' => true,
+                    'Access-Control-Max-Age' => 3600
+                ],
+            ],
+        ];
+
+    }
+
 }
